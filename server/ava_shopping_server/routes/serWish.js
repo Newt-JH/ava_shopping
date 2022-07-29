7741f3a76017abe2cbfa0db5c85afc872d11b19c
Newt-JH <flozzang@naver.com>
2022-07-29 14:54:34 +0900

    wish async await 처리 완료

--- server/ava_shopping_server/routes/serWish.js
+++ server/ava_shopping_server/routes/serWish.js
@@ -4,16 +4,34 @@ var router = express.Router();
 
 
 // 찜 목록 조회
-router.get('/user=:user&pro=:pro', function (req, res) {
+router.get('/user=:user&pro=:pro', async function (req, res) {
     userparams = req.params.user;
     proparams = req.params.pro;
-    db.wish(userparams,proparams,res)
+    try{
+        let f = await db.wish(userparams,proparams,res);
+        if(f[0] === undefined){
+            db.newWish(userparams,proparams);
+            res.send("찜 목록에 추가되었습니다.")
+        }else{
+            db.delWish(userparams,proparams);
+            res.send("찜 목록에서 삭제되었습니다.")
+        }
+    }catch(err){
+        res.send(err);
+    }
+
 });
 
 // 유저 찜목록 조회
-router.get('/select/:id', function (req, res) {
+router.get('/select/:id', async function (req, res) {
     user = req.params.id;
-    db.userWish(user,res)
+    try{
+        let f = await db.userWish(user,res);
+        res.send(f);
+    }catch(err){
+        res.send(err);
+    }
+    
 });
 
 module.exports = router;
--- server/ava_shopping_server/shopping_DB/serWishDB.js
+++ server/ava_shopping_server/shopping_DB/serWishDB.js
@@ -1,63 +1,39 @@
 const con = require('./DatabaseConn');
 const connection = con.dataCon;
+// pro > DB 읽어올때 쓰는 모듈 ( 프로미스 반환 / async await 사용하기 위해 사용 )
+// tto > row에 대해 읽어올 필요가 없는 쿼리 날릴때 사용
+const pro = con.pro;
+const tto = con.tto;
 
 
 // 찜 목록 조회 후 추가 or 삭제
-function wish(user,pro,res) {
-    const query = `select * from wish where userIndex = ${user} and proIndex = ${pro}`
-    connection.query(query,
-        (err, rows) => {
-            if (err) {
-                throw err;
-            }else if(rows[0] === undefined){
-                newWish(user,pro,res)
-            }else{
-                delWish(user,pro,res)
-            }
-        })
+function wish(user,proin) {
+    const query = `select * from wish where userIndex = ${user} and proIndex = ${proin}`;
+    return pro(query);
 }
 
 
 // 찜 목록 추가
-function newWish(user,pro,res) {
+function newWish(user,pro) {
     const query = `insert into wish(userIndex,proIndex) value(${user},${pro})`
-    connection.query(query,
-        (err) => {
-            if (err) throw err;
-            console.log("찜 추가 완료");
-            return res.json("찜 목록에 추가되었습니다.")
-        }
-    )
+    tto(query);
 }
 
 // 찜 목록 삭제
-function delWish(user,pro,res) {
+function delWish(user,pro) {
     const query = `delete from wish where userIndex = ${user} and proIndex = ${pro}`
-    connection.query(query,
-        (err) => {
-            if (err) throw err;
-            console.log("찜 삭제 완료");
-            return res.json("찜 목록에서 삭제되었습니다.")
-        }
-    )
+    tto(query);
 }
 
 // 유저 찜 목록
-function userWish(user,res){
+function userWish(user){
     const query = `select * from wish,product where wish.proIndex = product.proIndex and userIndex = ${user};`
-    connection.query(query,
-        (err,rows) => {
-            if (err) {throw err;}
-            else if(rows[0] === undefined){
-                return res.json(rows);
-            }else{
-                return res.json(rows);
-            }
-            
-        })
+    return pro(query);
 }
 
 module.exports = {
     wish,
-    userWish
+    userWish,
+    newWish,
+    delWish
 }
